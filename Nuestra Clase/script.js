0ea0b71a6513fc9411e42df0aa7d225cddc22cf2
zonzozon <87434882+zonzozon@users.noreply.github.com>
2023-03-26 20:15:13 +0200

    Language swicher (Future)

---
+++ Nuestra Clase/script.js
@@ -0,0 +1,9 @@
+function switchLanguage(lang) {
+    var currentUrl = window.location.href;
+    if (currentUrl.indexOf("?lang=") != -1) {
+      currentUrl = currentUrl.replace(/lang=\w\w(-\w\w)?/, "lang=" + lang);
+    } else {
+      currentUrl += "?lang=" + lang;
+    }
+    window.location.href = currentUrl;
+  }
